6cde813262c24d88293fa147468e44c05a3cbde3
giricken <147893420+giricken@users.noreply.github.com>
2023-12-09 11:10:17 +0900

    첫번째 커밋
    아으야

---
+++ main.py
@@ -0,0 +1,33 @@
+# S = input()
+# a = "abcdefghijklmnopqrstuvwxyz"
+
+# for i in a:
+    
+#     if i in S:
+#         print(S.index(i), end = " ")
+#     else:
+#         print("-1", end = " ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+S = int(input())
+Number = input()
+s = 0
+
+
+for i in number():
+    s +=int(i)
+print(s)
